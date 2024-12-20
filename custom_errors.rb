a5c03576d4efffa817c47b84e9f555d63cf5ba36
ChitrakCDB <bhattchitrak@gmail.com>
2024-12-20 10:48:17 +0530

    Feat: add custom error class and call it in the definition

---
+++ custom_errors.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+class NilInputError < StandardError; end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
+require './custom_errors'
 class StringCalculator
   def add(string)
+    raise NilInputError, 'Input cannot be nil' if string.nil?
     return 0 if string.empty?
 
     string.to_i
