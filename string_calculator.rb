d9d0654b3499f6c636c6f5725cb3638b186d50ae
ChitrakCDB <bhattchitrak@gmail.com>
2024-12-20 21:54:32 +0530

    Feat: add logic to handle new lines on tring calculator class

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 require './custom_errors'
+
 class StringCalculator
   def add(string)
     raise NilInputError, 'Input cannot be nil' if string.nil?
     return 0 if string.empty?
 
-    numbers = string.split(',').map(&:to_i)
+    numbers = extract_numbers(string)
     numbers.sum
+  end
+
+  def extract_numbers(string)
+    string.split(/[\n,]+/).map(&:to_i)
   end
 end
