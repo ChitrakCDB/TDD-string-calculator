88040db08d3bec2b013696367dda815faf827a17
ChitrakCDB <bhattchitrak@gmail.com>
2024-12-20 10:27:25 +0530

    Test: add case for empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,5 +3,12 @@
 require_relative '../string_calculator'
 describe StringCalculator do
   context '#add' do
+    let(:string_calculator) { StringCalculator.new }
+
+    context 'empty string' do
+      it 'should include 0 in output' do
+        expect(string_calculator.add('')).to eq(0)
+      end
+    end
   end
 end
